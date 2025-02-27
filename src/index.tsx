7190799a76ea7393915583988b85e799a78da67e
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-02-27 14:06:11 +0300

    22222мывмывм

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 
 
+
+
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<Provider store={store}>
